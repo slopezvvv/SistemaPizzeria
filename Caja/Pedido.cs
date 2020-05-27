695a67d98c8416cccc641266dfe1b75b52affa8e
yo <yo@DESKTOP-RVOAHLO>
2020-05-27 19:57:51 -0400

    Agregar archivos de proyecto.

---
+++ Caja/Pedido.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caja
+{
+    enum TipoEntrega
+    {
+        DESPACHO, LOCAL
+    }
+
+    enum TipoPago
+    {
+        EFECTIVO, TRANSFERENCIA
+    }
+
+    class Pedido
+    {
+        private readonly ArrayList _productos;
+        private double _monto;
+
+        public Pedido(ArrayList productos)
+        {
+            _monto = .0;
+            _productos = productos;
+            CalcularTotal();
+        }
+
+        // GETs y SETs
+        public ArrayList Productos => _productos;
+
+        public double Monto {
+            get => _monto;
+            private set
+            {
+                _monto = Math.Abs(value);
+            }
+        }
+
+        // CUSTOM
+        private void CalcularTotal()
+        {
+            // SUMATORIA
+            foreach (Producto p in _productos)
+                Monto = (p.Precio + Monto);
+        }
+    }
+}
---
+++ Caja/Producto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Caja
+{
+    enum TipoProducto
+    {
+        PROMO_MEDIANA_BEBIDA
+    }
+
+    class Producto
+    {
+        private double _precio;
+        private double _descuento;
+        private TipoProducto _tipoProducto;
+
+        public Producto()
+        {
+            _precio = 10000;
+            _descuento = .0;
+            _tipoProducto = TipoProducto.PROMO_MEDIANA_BEBIDA;
+        }
+
+        // GETs Y SETs
+        public double Descuento
+        {
+            get => _descuento;
+            set
+            {
+                if (value > 0 && value <= 100)
+                    _descuento = value;
+            }
+        }
+
+        public double Precio { get => _precio; set => _precio = value; }
+        internal TipoProducto TipoProducto { get => _tipoProducto; set => _tipoProducto = value; }
+    }
+}
